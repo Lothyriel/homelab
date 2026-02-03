ce372f383d9b7f9c4870dc848d22145d881eed5b
João Xavier <fastjonh@gmail.com>
2026-02-02 22:22:17 -0300

    add fail2ban configs (WIP)

--- setup.sh
+++ setup.sh
@@ -1,5 +1,14 @@
-ln -s ~/homelab/timers ~/.config/systemd/user
+# systemd services setup
 
-ln -s ~/homelab/quadlets ~/.config/containers/systemd
+ln -sf ~/homelab/timers ~/.config/systemd/user
+
+ln -sf ~/homelab/quadlets ~/.config/containers/systemd
 
 sudo loginctl enable-linger $USER
+
+
+# fail2ban setup
+# we are copying here instead of symlinking because selinux won't let fail2ban follow the symlink in our home folder
+sudo cp -r ~/homelab/fail2ban/* /etc/fail2ban/
+
+echo "Setup complete"
