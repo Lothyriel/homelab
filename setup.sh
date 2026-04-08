e3ed41fa54ec26bfad2c086e952c87378fdb3153
João Xavier <fastjonh@gmail.com>
2026-04-08 02:40:03 -0300

    chore: moving things

--- setup.sh
+++ setup.sh
@@ -10,5 +10,7 @@ sudo loginctl enable-linger $USER
 # fail2ban setup
 # we are copying here instead of symlinking because selinux won't let fail2ban follow the symlink in our home folder
 sudo cp -r ~/homelab/fail2ban/* /etc/fail2ban/
+# same ...
+sudo cp cockpit.conf /etc/cockpit/cockpit.conf
 
 echo "Setup complete"
